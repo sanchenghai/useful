e1d4573f461587d88b115878efbbab4784ff43f7
sanchenghai <539921311@qq.com>
2016-06-03 23:01:25 +0800

    Add files via upload

---
+++ JavaScript_the_good_parts/methods.js
@@ -0,0 +1,88 @@
+//标准方法，具体的在这里不写了，仅罗列一下
+
+/***
+
+Array
+
+array.concat(item...)
+array.join(separator)
+array.pop()
+array.push(item...)
+array.reverse()
+array.shift()
+array.slice(start, end)
+array.sort(comparefn)
+array.splice(start, deletCount, item...)
+array.unshift(item...)
+
+***/
+
+
+
+/***
+
+Function
+
+function.apply(thisArg, argArray)
+
+***/
+
+
+
+/***
+
+Number
+
+number.toExponential(fractionDigits)
+number.toFixed(fractionDigits)
+number.toPrecision(precision)
+number.toString(radix)
+
+***/
+
+
+
+/***
+
+Object
+
+object.hasOwnProperty(name)
+
+***/
+
+
+
+/***
+
+RegExp
+
+regexp.exec(string)
+regexp.text(string)
+
+***/
+
+
+
+/***
+
+String
+
+string.charAt(pos)
+string.charCodeAt(pos)
+string.concat(string...)
+string.indexOf(searchString, position)
+string.lastIndexOf(searchString, position)
+string.localeCompare(that)
+string.match(regexp)
+string.replace(searchValue, replaceValue)
+string.search(regexp)
+string.slice(start, end)
+string.split(separator, limit)
+string.substring(start, end)
+string.toLocaleLowerCase()
+string.toLocaleUpperCase()
+string.toLowerCase()
+string.toUpperCase()
+string.fromCharCode(char...)
+
+***/

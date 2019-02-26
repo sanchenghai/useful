54c8a8e4fdef728ead8c66776db7bce707a0f6cb
wangqin <539921311@qq.com>
2019-02-27 00:35:42 +0800

    python instance, dir, type

---
+++ python/isinstance.py
@@ -0,0 +1,60 @@
+使用type()
+
+首先，我们来判断对象类型，使用type()函数：
+
+基本类型都可以用type()判断：
+
+>>> type(123)
+<class 'int'>
+>>> type('str')
+<class 'str'>
+>>> type(None)
+<type(None) 'NoneType'>
+如果一个变量指向函数或者类，也可以用type()判断：
+
+>>> type(abs)
+<class 'builtin_function_or_method'>
+>>> type(a)
+<class '__main__.Animal'>
+
+
+
+使用isinstance()
+
+对于class的继承关系来说，使用type()就很不方便。我们要判断class的类型，可以使用isinstance()函数。
+ 总是优先使用isinstance()判断类型，可以将指定类型及其子类“一网打尽”。
+
+
+
+ 使用dir()
+
+如果要获得一个对象的所有属性和方法，可以使用dir()函数，它返回一个包含字符串的list，比如，获得一个str对象的所有属性和方法：
+
+>>> dir('ABC')
+['__add__', '__class__',..., '__subclasshook__', 'capitalize', 'casefold',..., 'zfill']
+类似__xxx__的属性和方法在Python中都是有特殊用途的，比如__len__方法返回长度。在Python中，如果你调用len()函数试图获取一个对象的长度，实际上，在len()函数内部，它自动去调用该对象的__len__()方法，所以，下面的代码是等价的：
+
+>>> len('ABC')
+3
+>>> 'ABC'.__len__()
+3
+
+
+
+小结
+
+通过内置的一系列函数，我们可以对任意一个Python对象进行剖析，拿到其内部的数据。要注意的是，只有在不知道对象信息的时候，我们才会去获取对象信息。如果可以直接写：
+
+sum = obj.x + obj.y
+就不要写：
+
+sum = getattr(obj, 'x') + getattr(obj, 'y')
+一个正确的用法的例子如下：
+
+def readImage(fp):
+    if hasattr(fp, 'read'):
+        return readData(fp)
+    return None
+假设我们希望从文件流fp中读取图像，我们首先要判断该fp对象是否存在read方法，如果存在，则该对象是一个流，如果不存在，则无法读取。hasattr()就派上了用场。
+
+请注意，在Python这类动态语言中，根据鸭子类型，有read()方法，不代表该fp对象就是一个文件流，它也可能是网络流，也可能是内存中的一个字节流，但只要read()方法返回的是有效的图像数据，就不影响读取图像的功能。

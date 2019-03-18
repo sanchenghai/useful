af6470d7e872c5e69b8cf619425a93e36b0c8e5d
wangqin <539921311@qq.com>
2019-03-19 00:17:48 +0800

    python pickling

---
+++ python/pickling.py
@@ -0,0 +1,25 @@
+pickle.dumps()方法把任意对象序列化成一个bytes，然后，就可以把这个bytes写入文件。或者用另一个方法pickle.dump()直接把对象序列化后写入一个file-like Object：
+
+当我们要把对象从磁盘读到内存时，可以先把内容读到一个bytes，然后用pickle.loads()方法反序列化出对象，也可以直接用pickle.load()方法从一个file-like Object中直接反序列化出对象。
+
+JSON表示的对象就是标准的JavaScript语言的对象，JSON和Python内置的数据类型对应如下：
+
+JSON类型	Python类型
+{}	dict
+[]	list
+"string"	str
+1234.56	int或float
+true/false	True/False
+null	None
+Python内置的json模块提供了非常完善的Python对象到JSON格式的转换。
+
+我们可以偷个懒，把任意class的实例变为dict：
+
+print(json.dumps(s, default=lambda obj: obj.__dict__))
+因为通常class的实例都有一个__dict__属性，它就是一个dict，用来存储实例变量。也有少数例外，比如定义了__slots__的class。
+
+小结
+
+Python语言特定的序列化模块是pickle，但如果要把序列化搞得更通用、更符合Web标准，就可以使用json模块。
+
+json模块的dumps()和loads()函数是定义得非常好的接口的典范。当我们使用时，只需要传入一个必须的参数。但是，当默认的序列化或反序列机制不满足我们的要求时，我们又可以传入更多的参数来定制序列化或反序列化的规则，既做到了接口简单易用，又做到了充分的扩展性和灵活性。

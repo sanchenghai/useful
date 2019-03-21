c53ca82bdf53793f642ad46171b53fa30657e1e1
wangqin <539921311@qq.com>
2019-03-22 00:31:48 +0800

    python multiprocessing

--- python/multiprocessing.py
+++ python/multiprocessing.py
@@ -5,3 +5,21 @@ Unix/Linux操作系统提供了一个fork()系统调用，它非常特殊。普�
 由于Windows没有fork调用，上面的代码在Windows上无法运行。由于Mac系统是基于BSD（Unix的一种）内核，所以，在Mac下运行是没有问题的，推荐大家用Mac学Python！
 
 有了fork调用，一个进程在接到新任务时就可以复制出一个子进程来处理新任务，常见的Apache服务器就是由父进程监听端口，每当有新的http请求时，就fork出子进程来处理新的http请求。
+
+Pool
+
+如果要启动大量的子进程，可以用进程池的方式批量创建子进程
+
+进程间通信
+
+Process之间肯定是需要通信的，操作系统提供了很多机制来实现进程间的通信。Python的multiprocessing模块包装了底层的机制，提供了Queue、Pipes等多种方式来交换数据。
+
+在Unix/Linux下，multiprocessing模块封装了fork()调用，使我们不需要关注fork()的细节。由于Windows没有fork调用，因此，multiprocessing需要“模拟”出fork的效果，父进程所有Python对象都必须通过pickle序列化再传到子进程去，所有，如果multiprocessing在Windows下调用失败了，要先考虑是不是pickle失败了。
+
+小结
+
+在Unix/Linux下，可以使用fork()调用实现多进程。
+
+要实现跨平台的多进程，可以使用multiprocessing模块。
+
+进程间通信是通过Queue、Pipes等实现的。

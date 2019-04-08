dd35b6914f284316ba5fa64f1d124234f7ac265a
wangqin <539921311@qq.com>
2019-04-09 00:03:39 +0800

    python XML

---
+++ python/xml.py
@@ -0,0 +1,7 @@
+DOM vs SAX
+
+操作XML有两种方法：DOM和SAX。DOM会把整个XML读入内存，解析为树，因此占用内存大，解析慢，优点是可以任意遍历树的节点。SAX是流模式，边读边解析，占用内存小，解析快，缺点是我们需要自己处理事件。
+
+正常情况下，优先考虑SAX，因为DOM实在太占内存。
+
+在Python中使用SAX解析XML非常简洁，通常我们关心的事件是start_element，end_element和char_data，准备好这3个函数，然后就可以解析xml了。

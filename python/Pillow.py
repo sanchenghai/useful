164a1fdb530d02a5997ddebf2afb02198e020e92
wangqin <539921311@qq.com>
2019-04-11 01:38:50 +0800

    python pillow

---
+++ python/Pillow.py
@@ -0,0 +1,11 @@
+PIL：Python Imaging Library，已经是Python平台事实上的图像处理标准库了。PIL功能非常强大，但API却非常简单易用。
+
+由于PIL仅支持到Python 2.7，加上年久失修，于是一群志愿者在PIL的基础上创建了兼容的版本，名字叫Pillow，支持最新Python 3.x，又加入了许多新特性，因此，我们可以直接安装使用Pillow。
+
+要详细了解PIL的强大功能，请请参考Pillow官方文档：
+
+https://pillow.readthedocs.org/
+
+小结
+
+PIL提供了操作图像的强大功能，可以通过简单的代码完成复杂的图像处理。
